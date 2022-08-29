453e0ef415d749b3ff00acb00f420a6d46b755c1
Dennys Lara <dennysjlarau@gmail.com>
2022-08-28 20:09:01 -0500

    mejora limpiando con caracteres especiales el nombre de la cancion y el cantante

--- ingesta/comun/cadena.py
+++ ingesta/comun/cadena.py
@@ -5,6 +5,20 @@ Funciones comunes para cadenas de texto o alfanumericos
 """
 from re import search
 
+CARACTERES_ESPECIALES = "!()/[]@"
+
+def limpiarCadena(cadena):
+    """
+    Función que limia caracteres especiales  
+    Argumentos:
+        cadena: cadena de texto alfanumérico
+    Retorna: cadena limpia de caracteres especias
+    """
+    cadena_limpia = cadena
+    for caracter in CARACTERES_ESPECIALES:
+        cadena_limpia = cadena_limpia.replace(caracter, "")
+    return cadena_limpia
+
 def contieneCadena(cadena, cadena_completa):
     """
     Función que compara cadena de textos  
@@ -13,9 +27,9 @@ def contieneCadena(cadena, cadena_completa):
         cadena_completa: cadena completa
     Retorna: True (si la cadena completa contiene la cadena simple)
     """
-    # transforma para discriminar minúsculas y mayúsculas
-    simple = cadena.casefold()
-    completa = cadena_completa.casefold()
+    # preprocesa cadenas antes de compararlos
+    simple = limpiarCadena(cadena.casefold())
+    completa = limpiarCadena(cadena_completa.casefold())
 
     # compara si la cadena completa contiene la cadena simple
     if search(simple, completa):
