53cc06bde86885c5250d910b2635421de2fc4238
Dennys Lara <dennysjlarau@gmail.com>
2022-09-10 14:54:56 -0500

    implementa obtención de letras de canciones desde yaletras.com

--- ingesta/letras_logica.py
+++ ingesta/letras_logica.py
@@ -170,3 +170,80 @@ def obtenerLetraConLetras4u(nombre_cancion, nombre_autor):
                 letra_cancion = letra_cancion + letra.text.replace('\n\n\n', '')    
 
     return letra_cancion
+
+def obtenerLetraConYaLetras(nombre_cancion, nombre_autor):
+    """
+    Función que extrae la letra de una canción desde YaLetras.com 
+    Argumentos:
+        nombre_cancion: nombre de la cancion
+        nombre_autor: nombre de autor o cantante
+    Retorna: letra de la canción o vacío
+    """
+    
+    WEB_YALETRAS = "https://www.yaletras.com/search?q="
+    letra_cancion = ''
+
+    # codifica el nombre de la canción y el nombre del cantante como parámetro de URL
+    cancion_param_url = cadena.prepararCadena(nombre_cancion).replace(' ', '+').replace('++', '+')
+    cantante_param_url = cadena.prepararCadena(nombre_autor).replace(' ', '+').replace('++', '+')
+    print('------------------')
+    print('cancion: ', cancion_param_url)
+    print('cantante: ', cantante_param_url)
+
+    # prepara URL de Lyrics para buscar letras por cancion y autor
+    cancion_autor_code_url = cancion_param_url + '+' + cantante_param_url
+    url_busqueda_letras4u = '%s%s' % (WEB_YALETRAS, cancion_autor_code_url) 
+    print('URL búsqueda en yaletras.com: ', url_busqueda_letras4u)
+    print('------------------')
+
+    # Obtiene el contenido web del resultado de búsqueda
+    page_yaletras = requests.get(url_busqueda_letras4u)
+    conten_yaletras = BeautifulSoup(page_yaletras.content, "html.parser")
+
+    # Obtiene el contenido del div principal donde está el resultado de búsqueda de letras4u
+    page_body = conten_yaletras.find(id="page_body")
+
+    #obtiene los tipo articles del contenido blog
+    articulos_blog = page_body.find_all("article")
+
+    # obtiene por cada sección de artículos el div principal, contenedor H3 y enlace a
+    url_letra_cancion = ''
+    for article in articulos_blog:
+        div_contenedor = article.find("div", class_="post-title-container")
+        h3 = div_contenedor.find("h3")
+        a = h3.find("a")
+        if (hasattr(a, 'text')):
+            cancion_autor = a.text.replace('\n', '')
+            link_letra = a.get("href") 
+            print('cancion autor: ', cancion_autor)
+            print('link letra: ', link_letra)
+
+            # valida autor y canción para obtener el enlace a la letra de la canción
+            if cadena.contieneCadena(nombre_cancion, cancion_autor) and cadena.contieneCadena(nombre_autor, cancion_autor):
+                url_letra_cancion =  link_letra
+            else:
+                print('autor y canción no coinciden')
+                print('-----------------')
+
+    # obtiene letra de canción
+    if url_letra_cancion == '':
+        print('No se pudo determinar la URL de la canción')
+        print('-----------------')
+    else:
+        print ('url letra canción: ', url_letra_cancion)
+        print('-----------------')
+
+        # obtiene contenido de la página web de la letra de la canción
+        page_letra_cancion = requests.get(url_letra_cancion)
+        contenido_letra_cancion = BeautifulSoup(page_letra_cancion.content, "html.parser")
+
+        # obtiene el div principal donde está la letra de la canción
+        div_body = contenido_letra_cancion.find("div", class_="post-body")
+
+        # obtiene el div contenedor de la letra de la canción
+        divs_style = div_body.find('div', attrs={'style': 'text-align: center;'})
+        for div in divs_style:
+            if (hasattr(div, 'text')):
+                letra_cancion = letra_cancion + div.text + ' '
+                
+    return letra_cancion
