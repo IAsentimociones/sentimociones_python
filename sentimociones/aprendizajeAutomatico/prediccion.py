4ad3a2ca419908ff8334f73ee0455d2d3c3aa9ce
Dennys Lara <dennysjlarau@gmail.com>
2022-09-24 18:45:43 -0500

    correccion de errores en la clasificacion de sentimiento registro de caja blanca

--- sentimociones/aprendizajeAutomatico/prediccion.py
+++ sentimociones/aprendizajeAutomatico/prediccion.py
@@ -6,8 +6,11 @@ Implementa funciones para la predicción de modelos de aprendizaje automáticos
 import time
 import sys
 sys.path.append(sys.path[0] + '/..')
-from cajaBlanca import auditoria
+from cajaBlanca import auditoria, logger
 from preprocesamiento import tokenizacion
+
+PROCESO_PRINCIPAL = 'Predicción ML'
+log = logger.configurar (PROCESO_PRINCIPAL + '.log', __name__)
 
 
 def predecirTexto (modelo, texto, vectorizador, modelos, cancion, cantante):
@@ -18,13 +21,18 @@ def predecirTexto (modelo, texto, vectorizador, modelos, cancion, cantante):
     Retorna: objeto del modelo con sus resultados
     """
     tiempo_inicio = time.time()
-    print(tiempo_inicio)
     features = tokenizacion.crear_caracteristica(texto, nrange=(1, 4))
     features = vectorizador.transform(features)
     prediction = modelo.predict(features)[0]
 
+    # valida que no tenga resultados y elimina en caso de tenerlo
+    indice_resultado = len(modelos[modelo.__class__.__name__])
+    if indice_resultado == 3:
+        modelos[modelo.__class__.__name__].pop(2)
+
     resultado = {}
     resultado['prediccion'] = prediction
+    log.debug('cancion:' + cancion + ' cantante: ' + cantante + ' modelo: ' + modelo.__class__.__name__ + ' predicción: ' + prediction)
     modelos[modelo.__class__.__name__].append(resultado)
     #print('Clasificación de sentimientos con ', modelo.__class__.__name__, ' : ', emoji_dict[prediction], modelos['SVC'][2])
     auditoria.registrarPistaSentimientosMultiples([tiempo_inicio, cancion, cantante, modelo.__class__.__name__, modelos[modelo.__class__.__name__]])
